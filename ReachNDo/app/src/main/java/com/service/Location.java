6356660c96c8cc589c5ff60b8ced6e7f29073870
francisco-rodrigues <rodrigues.lixa@gmail.com>
2015-09-09 17:00:01 +0100

    Class revision

--- ReachNDo/app/src/main/java/com/service/Location.java
+++ ReachNDo/app/src/main/java/com/service/Location.java
@@ -1,7 +1,52 @@
 package com.service;
 
 
-public class Location {
+import java.util.ArrayList;
 
+public class Location extends LocationCoords{
 
+    private String name;
+    private String user_description;
+    private double radius;
+    private ArrayList<Event> events;
+
+    public Location(double a, double b, String n, String u_d, double r) {
+        super(a, b);
+        this.name = n;
+        this.user_description = u_d;
+        this.radius = r;
+        events = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public void addEvent(Event e){
+        events.add(e);
+    }
+
+    public String getUser_description() {
+        return user_description;
+    }
+
+    public void setUser_description(String user_description) {
+        this.user_description = user_description;
+    }
 }
