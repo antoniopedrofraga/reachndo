2e3a46a7246966c26564018e4d98dd4222f4273f
João Nogueira <joao.nogueira01@gmail.com>
2015-09-12 14:53:05 +0100

    Already starts on boot

--- ReachNDo/app/src/main/java/com/service/AutoStart.java
+++ ReachNDo/app/src/main/java/com/service/AutoStart.java
@@ -4,16 +4,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Joao Nogueira on 11/09/2015.
  */
 public class AutoStart extends BroadcastReceiver
 {
+    @Override
     public void onReceive(Context arg0, Intent arg1)
     {
-        Intent intent = new Intent(arg0,LocationService.class);
-        arg0.startService(intent);
-        Log.i("Autostart", "started");
+        Intent i = new Intent(arg0, LocationService.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        arg0.startService(i);
     }
 }
